7f66b30293b42b81044f2881bab7b69f7bf998a4
Paul Golds <Paul.Golds@GMail.com>
2013-04-16 21:24:19 +0100

    VS project now has Unity references
    The VS project can now see Unity libraries, making all classes available.

---
+++ Sample Project- VS/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sample_Project__VS
+{
+    public class Class1
+    {
+        Unity
+    }
+}
